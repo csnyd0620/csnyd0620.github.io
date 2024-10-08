255dc9191ce25ea02381a37c6774d2481fcdff59
LokiOfJotunheim <ch.ul.snyder@gmail.com>
2024-10-08 15:50:45 -0400

    modified navBarPage.php

--- navBarPage.php
+++ navBarPage.php
@@ -1,15 +1,15 @@
 <?php 
     function createNavBar() {
-    echo '<div class="navBar">
-		        <a href="index.html">Home</a>
-		        <a href="aboutpage.html">About Me</a>
-		        <a href="workspage.html">Work Experience</a>
-		        <a href="projectpage.html">Project Examples</a>
-		        <a href="weatherpage.html">And Now, The Weather!</a>
-		        <a href="contactpage.html">Contact Me!</a>
-	    </div>
-	    <section class="main_content">
-	    <h5>This website is a beta-version and subject to change.</h5>
-	    <hr>';
+        echo "<div class="navBar">";
+        echo "<a href="index.html">Home</a>";
+		echo "<a href="aboutpage.html">About Me</a>";
+		echo "<a href="workspage.html">Work Experience</a>";
+		echo "<a href="projectpage.html">Project Examples</a>";
+	    echo "<a href="weatherpage.html">And Now, The Weather!</a>";
+		echo "<a href="contactpage.html">Contact Me!</a>";
+	    echo "</div>";
+	    echo "<section class="main_content">";
+	    echo "<h5>This website is a beta-version and subject to change.</h5>";
+	    echo "<hr>";
     }
 ?>
